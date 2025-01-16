3970f5bc43755bfb7676fdebc27e375699168bec
Seok-Hun <hamer1233@naver.com>
2025-01-16 15:58:59 +0900

    Feat : 크롬 드라이버 webdriver manager로 자동 생성

--- src/main/java/Indicators.java
+++ src/main/java/Indicators.java
@@ -6,27 +6,29 @@ import java.text.ParseException;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class Indicators {
-    // 크롬 드라이버 경로
-    private static final String WEB_DRIVER_PATH = "chromedriver.exe";
     private static final String RESOURCE_FILE_PATH = "AAFC SNS 경로.txt";
     private static final String RESULT_FILE_PATH = "결과.txt";
 
     public static void main(String[] args) throws IOException {
 
-        System.setProperty("webdriver.chrome.driver",WEB_DRIVER_PATH);
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
 
-        WebDriver driver = new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
         Actions actions = new Actions(driver);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         Path path = Paths.get(RESULT_FILE_PATH);
